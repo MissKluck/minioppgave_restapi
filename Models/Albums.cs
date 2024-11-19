429799fc25045747a59c8ababb6ad5155a6ded98
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-19 13:46:32 +0100

    added id

--- Models/Albums.cs
+++ Models/Albums.cs
@@ -1,5 +1,6 @@
 public class Albums
 {
+    public required int Id { get; set; }
     public required string Title { get; set; }
     public required string Artist { get; set; }
     public required int ReleaseYear { get; set; }
--- Models/Artists.cs
+++ Models/Artists.cs
@@ -1,5 +1,6 @@
 public class Artists
 {
+    public required int Id { get; set; }
     public required string Name { get; set; }
     public required string ActiveMembers { get; set; }
     public required int YearFormed { get; set; }
