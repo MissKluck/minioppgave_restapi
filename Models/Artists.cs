656f092b72602bc4f62e88b38035471871f70f7e
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-19 14:58:06 +0100

    added some unrequired fields

--- Models/Artists.cs
+++ Models/Artists.cs
@@ -2,8 +2,10 @@ public class Artists
 {
     public required int Id { get; set; }
     public required string Name { get; set; }
-    public required string ActiveMembers { get; set; }
-    public required int YearFormed { get; set; }
+    public bool IsDuo { get; set; }
+    public bool IsGroup { get; set; }
+    public string? Members { get; set; }
+    public int YearFormed { get; set; }
     public required string Genres { get; set; }
     public required string Labels { get; set; }
 }
