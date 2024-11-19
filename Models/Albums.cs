01899f6dc2b3de87f73b8d97731326febd29518c
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-19 13:33:27 +0100

    initial setup of album model

---
+++ Models/Albums.cs
@@ -0,0 +1,7 @@
+public class Albums
+{
+    public required string Title { get; set; }
+    public required string Artist { get; set; }
+    public required int ReleaseYear { get; set; }
+    public required string Genres { get; set; }
+}
