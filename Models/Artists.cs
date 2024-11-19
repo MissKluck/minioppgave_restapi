3c0c4adeceddb992f28ffd3cf6526ddba2d470df
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-19 13:40:04 +0100

    initial setup of artist model

---
+++ Models/Artists.cs
@@ -0,0 +1,8 @@
+public class Artists
+{
+    public required string Name { get; set; }
+    public required string ActiveMembers { get; set; }
+    public required int YearFormed { get; set; }
+    public required string Genres { get; set; }
+    public required string Labels { get; set; }
+}
