bd8ff68155fa19876c691829ea0c1c8d47e8c5ba
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-20 11:14:12 +0100

    commented out IsDuo

--- Models/Artists.cs
+++ Models/Artists.cs
@@ -2,7 +2,7 @@ public class Artists
 {
     public required int Id { get; set; }
     public required string Name { get; set; }
-    public bool IsDuo { get; set; }
+    //public bool IsDuo { get; set; }
     public bool IsGroup { get; set; }
     public string? Members { get; set; }
     public required int YearStarted { get; set; }
