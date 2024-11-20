94888bf77292bc24479979dafee02805fcb3bbc6
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-20 11:13:55 +0100

    fixed the route, removed IsDuo and fixed the null check in post method

--- Controllers/ArtistsController.cs
+++ Controllers/ArtistsController.cs
@@ -1,15 +1,14 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 
 namespace minioppgave_restapi;
 [ApiController]
-[Route("api/Albums")]
+[Route("api/artists")]
 
 public class ArtistsController : ControllerBase
 {
     private static List<Artists> artists = new List<Artists>{
         new Artists {Id = 1, Name = "Imagine Dragons", IsGroup = true, Members = "Dan Reynolds, Wayne Sermon, Ben McKee", YearStarted = 2008, Genres = new List<string> {"Pop rock, electropop, alternative rock"}},
-        new Artists {Id = 2, Name = "Daft Punk", IsDuo = true, Members = "Thomas Bangalter & Guy-Manuel de Homem-Christo", YearStarted = 1993, Genres = new List<string> {"French house, electronic, dance, disco"}},
+        new Artists {Id = 2, Name = "Daft Punk", IsGroup = true, Members = "Thomas Bangalter & Guy-Manuel de Homem-Christo", YearStarted = 1993, Genres = new List<string> {"French house, electronic, dance, disco"}},
         new Artists {Id = 3, Name = "Taylor Swift", IsGroup = false, YearStarted = 2003, Genres = new List<string> {"Country, pop, folk, rock"}},
         new Artists {Id = 4, Name = "Keane", IsGroup = true, Members = "Tom Chaplin, Tim Rice-Oxley, Richard Hughes, Jesse Quin", YearStarted = 1995, Genres = new List<string> {"alternate rock, indie, post-Britpop"}},
         new Artists {Id = 5, Name = "Susanne Sundfør", IsGroup = false, YearStarted = 2005, Genres = new List<string> {"Art pop, electronica, folk, experimental pop"}},
@@ -30,7 +29,7 @@ public class ArtistsController : ControllerBase
     [HttpPost]
     public IActionResult Post([FromBody] Artists _artists)
     {
-        if (artists == null)
+        if (_artists == null)
         {
             return BadRequest("Client error occured!");
         }
