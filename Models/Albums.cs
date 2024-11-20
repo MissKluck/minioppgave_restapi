eb3a03963e41162ebf7ad7a372faf0f6beb12a67
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-20 11:14:24 +0100

    added list to genres

--- Models/Albums.cs
+++ Models/Albums.cs
@@ -4,5 +4,5 @@ public class Albums
     public required string Title { get; set; }
     public required string Artist { get; set; }
     public required int ReleaseYear { get; set; }
-    public required string Genres { get; set; }
+    public required List<string> Genres { get; set; }
 }
