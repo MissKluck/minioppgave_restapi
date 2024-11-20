1e4a5c92ff84bc0e605607e0ea4270292baf1e26
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-20 11:10:43 +0100

    added albums and fixed null check in post method

--- Controllers/AlbumsController.cs
+++ Controllers/AlbumsController.cs
@@ -5,7 +5,16 @@ using Microsoft.AspNetCore.Mvc;
 public class AlbumsController : ControllerBase
 {
     private static List<Albums> _Albums = new List<Albums> {
-        new Albums {Id =, Title =, Artist =, ReleaseYear =, Genres =,}
+        new Albums {Id = 1, Title = "UNDERTALE Soundtrack", Artist = "Toby Fox", ReleaseYear = 2015, Genres = new List<string> {"Video game music", "chiptune"}},
+        new Albums {Id = 2, Title = "Fine Line", Artist = "Harry Styles", ReleaseYear = 2019, Genres = new List<string> {"Pop rock", "indie pop"}},
+        new Albums {Id = 3, Title = "Cause And Effect", Artist = "Keane", ReleaseYear =2019, Genres = new List<string> {"Alternative rock", "indie pop"}},
+        new Albums {Id = 4, Title = "The Gods We Can Touch", Artist = "AURORA", ReleaseYear = 2022, Genres = new List<string> {"Electropop", "folk-pop", "ethereal", "art pop"}},
+        new Albums {Id = 5, Title = "SOUR", Artist = "Olivia Rodrigo", ReleaseYear = 2021, Genres = new List<string> {"Pop", "alternative pop", "pop-punk"}},
+        new Albums {Id = 6, Title = "Wild World", Artist = "Bastille", ReleaseYear = 2016, Genres = new List<string> {"Alternative rock", "pop rock", "synth pop"}},
+        new Albums {Id = 7, Title = "The 2nd Law", Artist = "Muse", ReleaseYear = 2012, Genres = new List<string> {"Alternative rock", "art rock", "progressive rock", "electronic"}},
+        new Albums {Id = 8, Title = "Inception: Music from the Motion Picture", Artist = "Hans Zimmer", ReleaseYear = 2010, Genres = new List<string> {"Film score"}},
+        new Albums {Id = 9, Title = "Discovery", Artist = "Daft Punk", ReleaseYear = 2001, Genres = new List<string> {"French house", "disco", "nu-disco", "house"}},
+        new Albums {Id = 10, Title = "MANIA", Artist = "Fall Out Boy", ReleaseYear = 2018, Genres = new List<string> {"Pop rock", "electronic rock", "electropop"}}
     };
 
     [HttpGet]
@@ -17,7 +26,7 @@ public class AlbumsController : ControllerBase
     [HttpPost]
     public IActionResult Post([FromBody] Albums albums)
     {
-        if (albums == null)
+        if (_Albums == null)
         {
             return BadRequest("Client side error occured!");
         }
